6eea81e3c94a560aae175f8a4f1e0e43eae64ac3
Ageu Silva <39139593+Ageursilva@users.noreply.github.com>
2025-12-04 22:59:41 -0300

    add: ad-hoc 1026 e 2413

---
+++ Ad-Hoc/1026-Carrega ou não Carrega/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int t = int.Parse(Console.ReadLine());
+        int primeiro = 2 * (2 * t);
+
+        Console.WriteLine(primeiro);
+
+    }
+
+}
